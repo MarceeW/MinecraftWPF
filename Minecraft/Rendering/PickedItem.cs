1ab17776a5bbd09742ac6a6936f8b5fbf041cfe6
kaszinnyo <92094949+kaszinnyo@users.noreply.github.com>
2022-11-21 11:38:27 +0100

    PickedItem made to an independent class

---
+++ Minecraft/Rendering/PickedItem.cs
@@ -0,0 +1,22 @@
+﻿using Minecraft.Terrain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Minecraft.Rendering
+{
+    public class PickedItem
+    {
+        public ImageSource src;
+        public BlockType type;
+
+        public PickedItem(ImageSource src, BlockType type)
+        {
+            this.src = src;
+            this.type = type;
+        }
+    }
+}
