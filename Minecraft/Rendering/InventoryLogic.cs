ee0a036963d8988567881b680b183bffcfac3f5e
sandorkiss@stud.uni-obuda.hu <sandorkiss@stud.uni-obuda.hu>
2022-11-24 16:13:38 +0100

    InventoryLogic created with default interface implementation

---
+++ Minecraft/Rendering/InventoryLogic.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Minecraft.Rendering
+{
+    internal class InventoryLogic : IInventoryLogic
+    {
+        public void CreateInventory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HotbarMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InventoryItemMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnKeyDown(KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnMouseDown(MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnMouseEnterBlockImage(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnMouseLeaveBlockImage(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnMouseMove(MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetupHotbar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
